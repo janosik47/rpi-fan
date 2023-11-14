d1693b598e284cf486ebfc2672f8f615364a6204
Jacek Stania <38670505+janosik47@users.noreply.github.com>
2023-11-14 23:12:34 +0000

    Update fan.sh

--- fan.sh
+++ fan.sh
@@ -23,12 +23,12 @@ then
   echo 0 >  /sys/class/thermal/cooling_device0/cur_state
 
 # fan OFF and temp above the threshold - turn ON
-elif [[ $f1 -lt 1 && $t2 -gt $max_on ]]
+elif [[ $f1 -lt 1 && $t2 -ge $max_on ]]
 then 
   /usr/bin/logger -i "$prefix Current temp it $t2[C] > $max_on[C], turning fan ON"
   echo 1 >  /sys/class/thermal/cooling_device0/cur_state
 
 # keep the fan as it is  
 else 
-  /usr/bin/logger -i "$prefix Current temp is $t2 [C] (thresholds: OFF < $max_off[C] & ON > $max_on[C]) and the fan is $f2"
+  /usr/bin/logger -i "$prefix Current temp is $t2 [C] (thresholds: OFF<$max_off[C] & ON>=$max_on[C]) and the fan is $f2"
 fi
