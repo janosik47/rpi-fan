12d84f363af7fb51fc569337b2d573569b2f9e83
Jacek Stania <38670505+janosik47@users.noreply.github.com>
2023-11-14 22:28:15 +0000

    Update fan.sh

--- fan.sh
+++ fan.sh
@@ -3,6 +3,8 @@
 max_on=55
 max_off=50
 
+prefix="RPI Fan Service:"
+
 t1=$(cat /sys/class/thermal/thermal_zone0/temp)
 t2=$((t1/1000))
 
@@ -17,16 +19,16 @@ fi
 # fan ON and temp below the threshold - turn OFF
 if [[ $f1 -gt 0 && $t2 -lt $max_off ]]
 then
-  /usr/bin/logger -i "Current temp it $t2[C] < $max_off[C], turning fan OFF"
+  /usr/bin/logger -i "$prefix Current temp it $t2[C] < $max_off[C], turning fan OFF"
   echo 0 >  /sys/class/thermal/cooling_device0/cur_state
 
 # fan OFF and temp above the threshold - turn ON
 elif [[ $f1 -lt 1 && $t2 -gt $max_on ]]
 then 
-  /usr/bin/logger -i "Current temp it $t2[C] > $max_on[C], turning fan ON"
+  /usr/bin/logger -i "$prefix Current temp it $t2[C] > $max_on[C], turning fan ON"
   echo 1 >  /sys/class/thermal/cooling_device0/cur_state
 
 # keep the fan as it is  
 else 
-  /usr/bin/logger -i "Current temp is $t2 [C] (thresholds <$max_off[C] & >$max_on[C]) and the fan is $f2"
+  /usr/bin/logger -i "$prefix Current temp is $t2 [C] (thresholds <$max_off[C] & >$max_on[C]) and the fan is $f2"
 fi
